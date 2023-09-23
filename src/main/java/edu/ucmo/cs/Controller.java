46b6c0a7bd0f6c0832e64e09f91846aaf8e213c9
Satya's <kjavi119@gmail.com>
2023-09-22 22:52:39 -0500

    netfix

--- src/main/java/edu/ucmo/cs/Controller.java
+++ src/main/java/edu/ucmo/cs/Controller.java
@@ -9,31 +9,32 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/net/crud/")
 public class Controller {
     @Autowired
     private Repository repository;
 
-    @GetMapping("/movies")
+    @GetMapping("/get")
     public List<Netflix> getAllNetflixMovies() {
         return repository.findAll();
     }
 
-    @PostMapping("/movie")
+    @PostMapping("/post")
     public Netflix createNetflixMovie(@RequestBody Netflix netflix) {
         return repository.save(netflix);
     }
 
-    @PutMapping("/movies/{id}")
+    @PutMapping("/put/{id}")
     public ResponseEntity<Netflix> updateNetflixMovie(@PathVariable String id, @RequestBody Netflix netflixDetails) {
         Netflix netflix = repository.findById(id)
                 .orElseThrow(() -> new CustomException("Netflix not exist with id :" + id));
         netflix.setRelease_year(netflixDetails.getRelease_year());
+        netflix.setTitle(netflixDetails.getTitle());
         Netflix updatedNetflixMovie = repository.save(netflix);
         return ResponseEntity.ok(updatedNetflixMovie);
     }
 
-    @DeleteMapping("/movies/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteNetflixMovie(@PathVariable String id) {
         Netflix netflix = repository.findById(id)
                 .orElseThrow(() -> new CustomException("Department not exist with id :" + id));
